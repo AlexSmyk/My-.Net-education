17acecfbed0c2f1cab54ef03af56471c21ec5763
AlexSmyk1992 <alexolya5@mail.ru>
2022-09-03 12:15:20 +0300

    move my .net edu apps
    move my .net edu apps to separate repository

---
+++ ASP.NET/asp.net/CustomTemplateApp/CustomTemplateApp/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using CustomTemplateApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomTemplateApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult Display()
+        {
+            Book book = new Book { Name = "Война и мир", Id = 1, Price = 23.8m };
+            return View(book);
+        }
+
+        public ActionResult Edit()
+        {
+            Book book = new Book { Name = "Война и мир", Id = 1, Price = 23.8m };
+            return View(book);
+        }
+
+        public ActionResult Details ()
+        {
+            return View();
+        }
+
+        public ActionResult MyTemplate()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
